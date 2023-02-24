21f9942b2839d1c3e5f2bcf0065247f7b1a95816
Yuzhuo Zheng <yzheng10@unb.ca>
2023-02-24 17:36:39 -0400

    02/24 fix conflicts

--- main/src/Model/Course.java
+++ main/src/Model/Course.java
@@ -1,42 +1,43 @@
 package Model;
 
 public class Course {
-
+    private String code;
+    private String section;
+    private int creditHours;
     private String name;
+    private String description;
+    private String courseType;
+    //Techinically we could have a list of class times in some sort of structure but we might not wanna mess with that at this time.
 
-
-    public Course(String name) {
-
-        this.name = name;
-
+    public Course(){
     }
 
-    @Override
-    public String toString() {
-        return name;
+    public Course(String code, String section, int creditHours, String name, String description, String courseType){
+        this.code=code;
+        this.section=section;
+        this.creditHours=creditHours;
+        this.name=name;
+        this.description=description;
+        this.courseType=courseType;
     }
 
-    public String getName() {
-        return name;
-    }
+    public String getCode(){ return code; }
+    public void setCode(String code){ this.code=code; }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+    public String getSection(){ return section;	 }
+    public void setSection(String section){ this.section=section; }
 
-        Course course = (Course) o;
+    public int getCreditHours(){ return creditHours; }
+    public void setCreditHours(int creditHours){ this.creditHours=creditHours; }
 
-        return getName().equals(course.getName());
-    }
+    public String getName(){ return name; }
+    public void setName(String name){ this.name=name; }
 
-    @Override
-    public int hashCode() {
-        return getName().hashCode();
-    }
+    public String getDescription(){ return description; }
+    public void setDescription(String description){ this.description=description; }
 
-    public void updateCourseName(String name)
-    {
-        this.name = name;
-    }
+    public String getCourseType(){ return courseType; }
+    public void setCourseType(String courseType){ this.courseType=courseType;}
+
+    //Equals method needed?
 }
