1bd4531e1d47d5a101807ae41fc3cc2811b21b66
Luke <LukeLosier@outlook.com>
2023-03-01 19:43:47 -0400

    Added PreRecs to Course Updated Database Updated Enrollment by adding a default scheme and allowing the scheme to be edited. Created EnrollmentBuilder

--- main/src/Model/Course.java
+++ main/src/Model/Course.java
@@ -1,43 +1,49 @@
 package Model;
 
+import Model.Department;
 public class Course {
-    private String code;
-    private String section;
-    private int creditHours;
-    private String name;
-    private String description;
-    private String courseType;
-    //Techinically we could have a list of class times in some sort of structure but we might not wanna mess with that at this time.
+	private String code;
+	private String section;
+	private int creditHours;
+	private String name;
+	private String description;
+	private Department department;
+	private ArrayList<String> prerequisites;
+	//Techinically we could have a list of class times in some sort of structure but we might not wanna mess with that at this time.
 
-    public Course(){
-    }
+	public Course(){
+		prerequisites=new ArrayList<String>();
+	}
 
-    public Course(String code, String section, int creditHours, String name, String description, String courseType){
-        this.code=code;
-        this.section=section;
-        this.creditHours=creditHours;
-        this.name=name;
-        this.description=description;
-        this.courseType=courseType;
-    }
+	public Course(String code, String section, int creditHours, String name, String description, Department department, ArrayList<String> prerequisites){
+		this.code=code;
+		this.section=section;
+		this.creditHours=creditHours;
+		this.name=name;
+		this.description=description;
+		this.department=department;
+		this.prerequisites=prerequisites;
+	}
 
-    public String getCode(){ return code; }
-    public void setCode(String code){ this.code=code; }
+	public String getCode(){ return code; }
+	public void setCode(String code){ this.code=code; }
 
-    public String getSection(){ return section;	 }
-    public void setSection(String section){ this.section=section; }
+	public String getSection(){ return section;	 }
+	public void setSection(String section){ this.section=section; }
 
-    public int getCreditHours(){ return creditHours; }
-    public void setCreditHours(int creditHours){ this.creditHours=creditHours; }
+	public int getCreditHours(){ return creditHours; }
+	public void setCreditHours(int creditHours){ this.creditHours=creditHours; }
+	
+	public String getName(){ return name; }
+	public void setName(String name){ this.name=name; }
 
-    public String getName(){ return name; }
-    public void setName(String name){ this.name=name; }
+	public String getDescription(){ return description; }
+	public void setDescription(String description){ this.description=description; }
 
-    public String getDescription(){ return description; }
-    public void setDescription(String description){ this.description=description; }
+	public Department getDepartment(){ return department; }
+	public void setDepartment(Department department){ this.department=department;}
 
-    public String getCourseType(){ return courseType; }
-    public void setCourseType(String courseType){ this.courseType=courseType;}
-
-    //Equals method needed?
+	public ArrayList<String> getPreRequisites(){ return prerequisites; }
+	public void setPrerequisites(ArrayList<String> prerequisites){ this.prerequisites=prerequisites; }
+	//Equals method needed?
 }
