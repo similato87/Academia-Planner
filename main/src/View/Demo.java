97abd1e341eae2f79ea90de1245b4b0eedd089ea
CamilaPaez <97375203+CamilaPaez@users.noreply.github.com>
2023-03-25 00:10:53 -0300

    Update Demo.java

--- main/src/View/Demo.java
+++ main/src/View/Demo.java
@@ -17,15 +17,15 @@ public class Demo {
         // init two student
         Student s1 = new Student(3333333, "John Doe", "Fredericton", "Business");
         Student s2 = new Student(4444444, "Mary Smith", "Saint John", "Computer Science");
+        
+        // Yuzhuo Factory method
+        Department csdprt = new CSDepartmentFactory().createDepartment();
+        Department swedprt = new SWEDepartmentFactory().createDepartment();
 
         // init three course to show different prerequsites and departments
         Course c1 = new Course("CS1073", "FR01A", 4, "Java I", "Introduction to Java I", csdprt);
         Course c2 = new Course("CS1083", "FR01A", 4, "Java II", "Introduction to Java II", csdprt);
         Course c3 = new Course("SWE4403", "FR01A", 4, "Software Design Patterns and Architecture", "GoF software design patterns", swedprt);
-        
-        // Yuzhuo Factory method
-        Department csdprt = new CSDepartmentFactory().createDepartment();
-        Department swedprt = new SWEDepartmentFactory().createDepartment();
 
         // Luc builder
         EnrollmentBuilder enrollmentBuilder = new EnrollmentBuilder();
